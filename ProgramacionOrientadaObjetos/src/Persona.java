6ec9555a6960347917f17e57ed2e8c34b6fdfe12
JavierUgarrio <j.ugarrio.duro@gmail.com>
2024-09-18 10:04:55 +0200

    instaciar y crear primer objeto

--- ProgramacionOrientadaObjetos/src/Persona.java
+++ ProgramacionOrientadaObjetos/src/Persona.java
@@ -1,12 +1,12 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
- *
- * @author User
- */
 public class Persona {
+    //atributos
+    String nombre;
+    int edad;
+    
+    //metodos
+    void mostrarDatos(){
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Edad: " + edad);
+    }
     
 }
