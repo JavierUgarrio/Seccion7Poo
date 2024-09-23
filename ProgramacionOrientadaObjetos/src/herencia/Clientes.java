4934b0fbfcc5f9c1c8830f5ad6f4e931cab99ac6
JavierUgarrio <j.ugarrio.duro@gmail.com>
2024-09-23 12:15:06 +0200

    herencias2

---
+++ ProgramacionOrientadaObjetos/src/herencia/Clientes.java
@@ -0,0 +1,66 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package herencia;
+
+import java.util.Date;
+
+/**
+ *
+ * @author User
+ */
+public class Clientes extends Persona {
+    //Atributo
+    private int idCliente;
+    private Date fechaRegistro;
+    private boolean vip;
+    private static int contadorCliente;
+    
+    //Constructor
+    public Clientes(int idCliente, Date fechaRegistro, boolean vip, String nombre, int edad, String apellidos, char genero, String direccion) {
+        super(nombre, edad, apellidos, genero, direccion);
+        this.idCliente = ++Clientes.contadorCliente;
+        this.fechaRegistro = fechaRegistro;
+        this.vip = vip;
+    }
+    
+    //Get & Set
+
+    public int getIdCliente() {
+        return this.idCliente;
+    }
+
+    public Date getFechaRegistro() {
+        return this.fechaRegistro;
+    }
+
+    public void setFechaRegistro(Date fechaRegistro) {
+        this.fechaRegistro = fechaRegistro;
+    }
+
+    public boolean isVip() {
+        return this.vip;
+    }
+
+    public void setVip(boolean vip) {
+        this.vip = vip;
+    }
+
+   //toString
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Clientes{");
+        sb.append("idCliente=").append(idCliente);
+        sb.append(", fechaRegistro=").append(fechaRegistro);
+        sb.append(", vip=").append(vip);
+        sb.append(" , ").append(super.toString());
+        sb.append('}');
+        return sb.toString();
+    }
+   
+    
+    
+}
