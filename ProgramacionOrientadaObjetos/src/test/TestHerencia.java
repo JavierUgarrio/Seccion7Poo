725e4dea1e67de23ff799cb1f1c27842f82bafe8
JavierUgarrio <j.ugarrio.duro@gmail.com>
2024-09-23 12:38:45 +0200

    test

--- ProgramacionOrientadaObjetos/src/test/TestHerencia.java
+++ ProgramacionOrientadaObjetos/src/test/TestHerencia.java
@@ -4,6 +4,9 @@
  */
 package test;
 import herencia.Persona;
+import herencia.Empleado;
+import herencia.Clientes;
+import java.util.*;
 /**
  *
  * @author User
@@ -12,5 +15,14 @@ public class TestHerencia {
    public static void main(String [] args){
        Persona persona1 = new Persona("Javier",12,"Perez",'M',"calle mayor 15 Leon");
        System.out.println(persona1);
+       
+       Empleado empleado1 = new Empleado(1,2000,"Jose Luis");
+       System.out.println(empleado1);
+       Empleado empleado2 = new Empleado(2,1500,"Maria");
+       System.out.println(empleado2);
+       
+       var fecha = new Date();
+       Clientes cliente1 = new Clientes(1, fecha, true,"Alvaro",12,"Moreno",'M',"calle de la cordura Grandada");
+       System.out.println(cliente1);
    } 
 }
