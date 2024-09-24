3e0ed2742789258c3cf3683d3aef043ed2236a28
JavierUgarrio <j.ugarrio.duro@gmail.com>
2024-09-24 11:33:04 +0200

    clases envolventes, parseint, parsedouble

---
+++ ProgramacionOrientadaObjetos/src/test/TestEnvolventes.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package test;
+
+/**
+ *
+ * @author User
+ */
+public class TestEnvolventes {
+    public static void main(String [] args){
+        int n1 = 10;
+        Integer n =10;
+        System.out.println("Entero -> " + n+ " -> "+ (n+n));
+        String nveces = n.toString();
+        System.out.println("Entero a String -> " + nveces + " -> "+ (nveces +nveces));
+        
+    }
+}
