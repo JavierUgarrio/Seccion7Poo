dac6cd30cde58ec868f3f8248020ee842ccaa7c0
JavierUgarrio <j.ugarrio.duro@gmail.com>
2024-09-24 10:06:03 +0200

    enum y testenum

---
+++ ProgramacionOrientadaObjetos/src/test/TestEnum.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package test;
+import enumeraciones.Dias;
+
+public class TestEnum {
+   public static void main(String [] args){
+       System.out.println(Dias.LUNES);
+       System.out.println(Dias.DOMINGO);
+   } 
+}
