de59af31ddbd6dc69a345fc8ea447a9f4ad7a6be
JavierUgarrio <j.ugarrio.duro@gmail.com>
2024-09-24 11:23:04 +0200

    inicializar bloque codigo

---
+++ ProgramacionOrientadaObjetos/src/bloque/codigo/TestBloque.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package bloque.codigo;
+
+/**
+ *
+ * @author User
+ */
+public class TestBloque {
+    public static void main(String []args ){
+        Persona p1 = new Persona();
+        Persona p2 = new Persona();
+        System.out.println(p1);
+        System.out.println(p2);
+    }
+    
+}
