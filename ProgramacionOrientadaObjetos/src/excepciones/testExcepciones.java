4800a21191e5acfa8965bafc40ee49195308f34d
JavierUgarrio <j.ugarrio.duro@gmail.com>
2024-09-25 12:38:08 +0200

    excepciones

---
+++ ProgramacionOrientadaObjetos/src/excepciones/testExcepciones.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package excepciones;
+
+import java.util.*;
+
+/**
+ *
+ * @author User
+ */
+public class testExcepciones {
+    public static void main(String [] args){
+        Scanner leer = new Scanner(System.in);
+        
+        try{
+          System.out.println("Ingrese n1: ");
+            int n1 = leer.nextInt();
+            System.out.println("Ingrese n2: ");
+            int n2 = leer.nextInt();
+            int resultado = n1/n2;
+            System.out.println(resultado);
+        }catch(java.util.InputMismatchException e){
+            System.err.println("Ingrese solo numeros enteros");
+            e.printStackTrace(System.out);
+        }
+        
+        
+        
+    }
+}
