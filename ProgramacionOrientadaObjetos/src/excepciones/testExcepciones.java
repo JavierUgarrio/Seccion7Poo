7680a41cacbee81aec4f145d63327fc0cc38b11d
JavierUgarrio <j.ugarrio.duro@gmail.com>
2024-09-25 12:53:56 +0200

    multiples excepciones

--- ProgramacionOrientadaObjetos/src/excepciones/testExcepciones.java
+++ ProgramacionOrientadaObjetos/src/excepciones/testExcepciones.java
@@ -13,27 +13,30 @@ import java.util.*;
 public class testExcepciones {
     public static void main(String [] args){
         Scanner leer = new Scanner(System.in);
+        boolean continuarEjeccucion = true;
         
-        try{
-          System.out.println("Ingrese n1: ");
-            int n1 = leer.nextInt();
-            System.out.println("Ingrese n2: ");
-            int n2 = leer.nextInt();
-            int resultado = n1/n2;
-            System.out.println(resultado);
-        }catch(java.util.InputMismatchException e){
-            System.err.println("Ocurrio un error: Ingrese solo numeros enteros");
-            e.printStackTrace(System.out);
-        }catch(ArithmeticException e){
-            System.out.println("Ocurrio un error: No se puede dividir entre 0");
-            e.printStackTrace(System.out);
-        }catch(Exception e){
-            e.printStackTrace(System.out);
-        }finally{
-            System.out.println("Se reviso la división");
-        }
-        
-        
+        do{
+             try {
+                System.out.println("Ingrese n1: ");
+                int n1 = leer.nextInt();
+                System.out.println("Ingrese n2: ");
+                int n2 = leer.nextInt();
+                int resultado = n1 / n2;
+                System.out.println(resultado);
+                continuarEjeccucion = false;
+            } catch (java.util.InputMismatchException e) {
+                System.err.println("Ocurrio un error: Ingrese solo numeros enteros");
+                e.printStackTrace(System.out);
+                leer.nextLine();
+            } catch (ArithmeticException e) {
+                System.out.println("Ocurrio un error: No se puede dividir entre 0");
+                e.printStackTrace(System.out);
+            } catch (Exception e) {
+                e.printStackTrace(System.out);
+            } finally {
+                System.out.println("Se reviso la division");
+            }
+        }while(continuarEjeccucion);
         
     }
 }
